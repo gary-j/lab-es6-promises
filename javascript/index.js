3e661fee0bc7c47d64dd9df0d73ff2e3c3f01fd1
Dev deRev <devderev@free.fr>
2022-03-14 18:05:20 +0100

    iteration 2

--- javascript/index.js
+++ javascript/index.js
@@ -4,21 +4,21 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
+  // getInstruction("mashedPotatoes", 0, (step1) => {
+  //   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+  // }, (error) => console.log(error));
   
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
+  // getInstruction("mashedPotatoes", 1, (step2) => {
+  //   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+  // }, (error) => console.log(error));
   
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
+  // getInstruction("mashedPotatoes", 2, (step3) => {
+  //   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+  // }, (error) => console.log(error));
   
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
+  // getInstruction("mashedPotatoes", 3, (step4) => {
+  //   document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+  // }, (error) => console.log(error));
   
   getInstruction("mashedPotatoes", 4, (step5) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
@@ -28,9 +28,63 @@
 
 
 // Iteration 1 - using callbacks
+
+ getInstruction("mashedPotatoes", 0, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 1, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+      getInstruction("mashedPotatoes", 2, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+        getInstruction("mashedPotatoes", 3, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+          getInstruction("mashedPotatoes", 4, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+            document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+
+          }, (error) => console.log(error));
+        }, (error) => console.log(error));
+      }, (error) => console.log(error));
+    }, (error) => console.log(error));
+  }, (error) => console.log(error));
+
 // ...
 
 // Iteration 2 - using promises
+// for(let i = 0; i<steak.length; i++){
+  let leSteak = document.querySelector("#steak")
+  obtainInstruction("steak", 0).then((instruction)=>{
+  leSteak.innerHTML += `<li>${instruction}</li>`;
+  return obtainInstruction('steak', 1);
+  }
+  ).then(
+    (instruction)=>{
+    leSteak.innerHTML += `<li>${instruction}</li>`;
+    return obtainInstruction('steak', 2);
+    }).then(
+      (instruction)=>{
+        leSteak.innerHTML += `<li>${instruction}</li>`;
+        return obtainInstruction('steak', 3);
+      }).then(
+        (instruction)=>{
+          leSteak.innerHTML += `<li>${instruction}</li>`;
+          return obtainInstruction('steak', 4);
+        }).then(
+          (instruction)=>{
+            leSteak.innerHTML += `<li>${instruction}</li>`;
+            return obtainInstruction('steak', 5);
+          }).then(
+            (instruction)=>{
+              leSteak.innerHTML += `<li>${instruction}</li>`;
+              return obtainInstruction('steak', 7);
+            }).then(
+              (instruction)=>{
+                leSteak.innerHTML += `<li>${instruction}</li>`;
+              }).then(
+                ()=>leSteak.innerHTML += `<li>Stake is ready!</li>`
+                )
+
+// }
 // ...
 
 // Iteration 3 using async/await
